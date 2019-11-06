d35474b15e2e62e4f0d2930e479b37fcdbbae537
jiachin1995 <jiachin1995@gmail.com>
2019-11-06 22:07:53 +0800

    Update q1.py
    No need to normalise inputs!

--- Project2/part B/q1.py
+++ Project2/part B/q1.py
@@ -79,7 +79,7 @@ def read_data_chars():
   print(x_train)
   print(np.amax(x_train))
   
-  """NEED to normalise x inputs"""
+  """Note: NO need to normalise x inputs"""
   
   return x_train, y_train, x_test, y_test
 
