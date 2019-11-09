1633c5279576bd45dfa68ee539b40fdc136be73d
Ching Jia Chin <jiachin1995@gmail.com>
2019-11-10 00:09:51 +0800

    added RNN

---
+++ Project2/part B/q5.py
@@ -0,0 +1,52 @@
+import pylab as plt
+
+test_acc = [
+    0.5985714,
+    0.85,
+    0.69142854,
+    0.7871429,
+    0.6414286,
+    0.9028571,
+    0.66,
+    0.89,
+]
+
+runtimes = [
+    1106,
+    183,
+    423,
+    141,
+    1030,
+    186,
+    425,
+    138,
+]
+
+
+labels = [
+    "Q1",
+    "Q2",
+    "Q3",
+    "Q4",
+    "Q1&dropouts",
+    "Q2&dropouts",
+    "Q3&dropouts",
+    "Q4&dropouts",
+]
+
+fig, ax1 = plt.subplots()
+
+ax2 = ax1.twinx()
+ax1.plot(labels, test_acc, 'r-')
+ax2.plot(labels, runtimes, 'b-')
+
+ax1.set_xlabel('X data')
+ax1.set_ylabel('test accuracy', color='red')
+ax2.set_ylabel('Run Time (seconds)', color='b')
+
+
+
+
+plt.savefig('figures/q5.png')
+plt.show()
+
